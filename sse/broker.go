9db1575cf10d3a8798add0993956735255fdcb02
Pablo Pavan <adeliaopavan@gmail.com>
2026-01-14 20:24:57 -0300

    broker

---
+++ sse/broker.go
@@ -0,0 +1,19 @@
+package sse
+
+import "context"
+
+type BrokerMsg struct {
+	Pattern string
+	Channel string
+	Payload []byte
+}
+
+type Subscription interface {
+	Channel() <-chan BrokerMsg
+	Close() error
+}
+
+type Broker interface {
+	Subscribe(ctx context.Context, pattern ...string) (Subscription, error)
+	Publish(ctx context.Context, channel string, payload []byte) error
+}
